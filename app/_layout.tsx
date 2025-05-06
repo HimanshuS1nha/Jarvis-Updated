4bf1f1636a125bb5300ee7b86dbdba52d6e151b8
Himanshu Sinha <ashusinha1904@gmail.com>
2025-05-06 20:30:40 +0530

    React Query setup complete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
 import { Stack } from "expo-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import { useTheme } from "@/hooks/use-theme";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const theme = useTheme((state) => state.theme);
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        statusBarStyle: theme === "light" ? "dark" : "light",
-        statusBarBackgroundColor: theme === "light" ? "#fff" : "#000",
-      }}
-    />
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          statusBarStyle: theme === "light" ? "dark" : "light",
+          statusBarBackgroundColor: theme === "light" ? "#fff" : "#000",
+        }}
+      />
+    </QueryClientProvider>
   );
 }
